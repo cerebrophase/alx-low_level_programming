92efbf012b2f28c343f4fa85fe9b3c1dd1ad29f6
lawal bashir <lawalmuhdbashir@gmail.com>
2022-12-11 08:55:26 -0800

    task 11, print from 00 to 89

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,12 +9,12 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i <= 8; i++)
 	{
 		for (j = 1; j <= 9; j++)
 		{
-			putchar((i % 10)+ '0');
-			putchar((j % 10)+ '0');
+			putchar((i % 10) + '0');
+			putchar((j % 10) + '0');
 			if (i != 9 || j != 9)
 			{
 				putchar(',');
