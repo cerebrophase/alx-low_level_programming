107745802a7edb9ea747da86d9fabf4955baa64b
lawal bashir <lawalmuhdbashir@gmail.com>
2022-12-13 12:28:05 -0800

    task 00

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * void print_alphabet_x10(void) - prints all the alphabets ten times
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,9 @@
 #ifndef _main_h_
 #define _main_h_
 
-int _putchar(char);
+int _putchar(char c);
 void print_alphabet(void);
+void print_alphabet_x10(void);
 
 #endif
 
