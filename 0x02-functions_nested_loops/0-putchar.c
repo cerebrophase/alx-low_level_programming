126c295eaaa6cf32e18d6d068ccda5daa256e9c7
lawal bashir <lawalmuhdbashir@gmail.com>
2022-12-13 05:59:25 -0800

    task 0 correction

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - prints _putchar followed by a newline
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+int _putchar(void)
 {
 	printf("_putchar\n");
 	return (0);
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+
+	int print_alphabet;
+	_putchar;
+	return (0);
+}
