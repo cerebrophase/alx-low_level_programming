8c35cb8fcae045c14903bafd772ea3cc599f905d
lawal bashir <lawalmuhdbashir@gmail.com>
2022-12-13 13:52:38 -0800

    task 1

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,21 @@
 #include "main.h"
-
 /**
- * void print_alphabet_x10(void) - prints all the alphabets ten times
+ * print_alphabet_x10 - Prints the alphabet 10 times.
  *
- * Return: 0
+ * Return: Always 0 (Success)
  */
 void print_alphabet_x10(void)
 {
+	int a, b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 97; b <= 122; b++)
+		{
+			_putchar(b);
+		}
+	_putchar('\n');
+	}
+}
+
 
