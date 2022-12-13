7387fee16bc7f48883a4af0c05d6b2bd3aec38c9
lawal bashir <lawalmuhdbashir@gmail.com>
2022-12-13 14:53:55 -0800

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * int _abs - prints the absolute value of an integer
+ *
+ * @r: integer to check for absolute value
+ *
+ * Return: 0
+ */
+int _abs(int r)
+{
+	if (r < 0)
+	{
+		return (r * -1);
+	}
+	return (r);
+}
