6fcd36cdd047ad78ac22087c3757f1a7ff9ac030
lawal bashir <lawalmuhdbashir@gmail.com>
2022-12-15 03:46:48 -0800

    task 0. multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * main - test function if positive or negative
- *
- * Return: 0
- */
+  * main - Test function for positive or negative
+  * Return: 0
+  */
+
 int main(void)
 {
 	int i;
 
-	i = 98;
+	i = 0;
 	positive_or_negative(i);
-	
-	return (0);
+
+return (0);
 }
