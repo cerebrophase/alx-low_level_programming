f3aa002bbc4d54380e556be88c509510ad2f2602
lawal bashir <lawalmuhdbashir@gmail.com>
2022-12-15 22:26:30 -0800

    task 9. Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,23 +13,25 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	i = 1;
+	printf("%d", i);
+	for (i = 2; i <= 100; i++)
 	{
-		if (i % 15 == 0)
+		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf(" FizzBuzz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf(" Buzz");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf(" Fizz");
 		}
 		else
 		{
-			printf("%d ", i);
+			printf(" %d", i);
 		}
 	}
 	printf("\n");
