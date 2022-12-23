47dce4e69e48402e4d4fde848b6b13843fdfec7e
lawal bashir <lawalmuhdbashir@gmail.com>
2022-12-23 01:09:37 -0800

    a function that capitalizes all words of a string.

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,19 +10,24 @@
 
 char *cap_string(char *s)
 {
-	int i;
-	int len = strlen(s);
+	int count = 0, i;
+	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-	s[0] = toupper(s[0]);
-
-	for (i = 1; i < len; i++)
+	if (*(s + count) >= 97 && *(s + count) <= 122)
+		*(s + count) = *(s + count) - 32;
+	count++;
+	while (*(s + count) != '\0')
 	{
-		if (s[i] == ' ' || s[i] == '\n' || s[i] == ',' || s[i] == ';'
-|| s[i] == '.' || s[i] == '!' || s[i] == '?' || s[i] == '"'
-|| s[i] == '(' || s[i] == ')' || s[i] == '{' || s[i] == '}')
+		for (i = 0; i < 13; i++)
 		{
-			s[i + 1] = toupper(s[i + 1]);
+			if (*(s + count) == sep_words[i])
+			{
+				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+					*(s + (count + 1)) = *(s + (count + 1)) - 32;
+				break;
+			}
 		}
+		count++;
 	}
 	return (s);
 }
