5c046f25301aecb3aff278bc767b358c5fb79ec3
lawal bashir <lawalmuhdbashir@gmail.com>
2023-01-12 14:58:26 -0800

    correction to 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,32 +11,34 @@
 
 int **alloc_grid(int width, int height)
 {
-	int **grid;
+	int **gridout;
 	int i, j;
 
 	if (width < 1 || height < 1)
 		return (NULL);
 
-	grid = malloc(height * sizeof(int *));
-	if (grid == NULL)
+	gridout = malloc(height * sizeof(int *));
+	if (gridout == NULL)
 	{
-		free(grid);
+		free(gridout);
 		return (NULL);
 	}
+
 	for (i = 0; i < height; i++)
 	{
-		grid[i] = malloc(width * sizeof(int));
-		if (grid[i] == NULL)
+		gridout[i] = malloc(width * sizeof(int));
+		if (gridout[i] == NULL)
 		{
-			for (j = 0; j < 1; j++)
-				free(grid[j]);
-			free(grid);
+			for (i--; i >= 0; i--)
+				free(gridout[i]);
+			free(gridout);
 			return (NULL);
 		}
-		for (j = 0; j < width; j++)
-		{
-			grid[i][j] = 0;
-		}
 	}
-	return (grid);
+
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			gridout[i][j] = 0;
+
+	return (gridout);
 }
