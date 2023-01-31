e15d17ad21d63b8ed0c4eb212e93a8a2fb5f07b4
lawal bashir <lawalmuhdbashir@gmail.com>
2023-01-31 08:18:32 -0800

    a function that adds a new node at the beginning of a listint_t list.

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ *  * add_nodeint - add a new node at the beginning of a listint_t list
+ *   * @head: double pointer to head of list
+ *    * @n: integer data for new node
+ *     *
+ *      * Return: address of new node, or NULL if failed
+ *       */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	    listint_t *new;
+
+	        new = malloc(sizeof(listint_t));
+		    if (!new)
+			            return (NULL);
+
+		        new->n = n;
+			    new->next = *head;
+			        *head = new;
+
+				    return (new);
+}
+
