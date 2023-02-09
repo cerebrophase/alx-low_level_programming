edadace5a7adcbd0a07b594b0efbba5c94d195d7
lawal bashir <lawalmuhdbashir@gmail.com>
2023-02-09 00:06:36 -0800

    correction to task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,27 +18,25 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t lenr, lenw;
 	char *buffer;
 
-	if (filename == NULL)
+	if (!filename)
 		return (0);
-	fd = open(filename, 0_RDONLY);
+
+	fd = open(filename, O_RDONLY);
+
 	if (fd == -1)
 		return (0);
+
 	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-	{
-		close(fd);
+	if (!buffer)
 		return (0);
-	}
+
 	lenr = read(fd, buffer, letters);
-	close(fd);
-	if (lenr == -1)
-	{
-		free(buffer);
-		return (0);
-	}
 	lenw = write(STDOUT_FILENO, buffer, lenr);
+
+	close(fd);
+
 	free(buffer);
-	if (lenr != lenw)
-		return (0);
+
 	return (lenw);
 }
+
